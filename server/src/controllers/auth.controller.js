cc6c8a39ef15d7ab88a232fe22fdfb076a396422
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-02-21 13:18:10 -0800

    fin server

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 const signupController = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log(req.body);
     const user = await User.create({ name, email, password });
     res.status(200).json(user);
   } catch (error) {
